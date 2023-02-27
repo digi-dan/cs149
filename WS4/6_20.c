7a916437540a57898b25150b66adde7298c478ae
digi-dan <danny.garcia@sjsu.edu>
2023-02-27 13:11:46 -0800

    Documentation for C files

--- WS4/6_20.c
+++ WS4/6_20.c
@@ -1,21 +1,27 @@
+/**
+*	Description: Reads in the user's inputted amount of laps and outputs the 
+*	miles
+*	Author name: Danny Garcia
+*	Author email: danny.garcia@sjsu.edu
+*	Last modified Date: 02-27-2023
+**/
 #include <stdio.h>
 
 /* Converts laps into miles */
 double LapsToMiles(double userLaps){
 
-const int LAPS_PER_MILE = 4;
-double miles = userLaps / LAPS_PER_MILE;
+	const int LAPS_PER_MILE = 4;
+	double miles = userLaps / LAPS_PER_MILE;
 
-return miles;
+	return miles;
 }
 
 int main(void) {
+	/*Scan User's laps, then convert and output the laps into miles*/
+	double userLaps;
 
-   /* Type your code here. Your code must call the function.  */
-double userLaps;
+	scanf("%lf", &userLaps);
 
-scanf("%lf", &userLaps);
-
-printf("%0.2lf\n", LapsToMiles(userLaps));
-   return 0;
+	printf("%0.2lf\n", LapsToMiles(userLaps));
+	return 0;
 }
--- WS4/6_30.c
+++ WS4/6_30.c
@@ -1,41 +1,62 @@
+/**
+*	Description: Reads in 4 integers and returns the
+*	biggest integer and the smallest integer
+*	Author Name: Danny Garcia
+*	Author email: danny.garcia@sjsu.edu
+*	Last Modified Date: 02/27/2023
+**/
 #include <stdio.h>
 
+/*Function that takes in 4 integers and returns the largest integer*/
 int MaxNumber(int num1, int num2, int num3, int num4){
-int max = num1;
-if(num2 > max){
-max = num2;
-}
-if(num3 > max){
-max = num3;
-}
-if(num4 > max){
-max = num4;
-}
-return max;
+	
+	// intialize variable max to first integer
+	int max = num1;
+	
+	// Compare max with rest of input
+	if(num2 > max){
+		max = num2;
+	}
+	if(num3 > max){
+		max = num3;
+	}
+	if(num4 > max){
+		max = num4;
+	}
+	
+	// Return maximum number
+	return max;
 }
 
+/*Functio that takes in 4 inegers and returns the smallest integer*/
 int MinNumber(int num1, int num2, int num3, int num4){
-int min = num1;
-if(num2 < min){
-min = num2;
-}
-if(num3 < min){
-min = num3;
-}
-if(num4 < min){
-min = num4;
-}
-return min;
+	
+	// Initalize variable min to first inputted integer
+	int min = num1;
+	
+	// Compare min with rest of input
+	if(num2 < min){
+		min = num2;
+	}
+	if(num3 < min){
+		min = num3;
+	}
+	if(num4 < min){
+		min = num4;
+	}
+	
+	// Return minimum number
+	return min;
 }
 int main(void) {
+	
+	// Initialize input variables and scan for input
+	int num1, num2, num3, num4;
+	scanf("%d%d%d%d", &num1, &num2, &num3, &num4);
+	
+	// Call and print the Min and Max function
+	printf("Maximum is %d\n", MaxNumber(num1, num2, num3, num4));
+	printf("Minimum is %d\n", MinNumber(num1, num2, num3, num4));
 
-   /* Type your code here. Your code must call the function.  */
-int num1, num2, num3, num4;
-scanf("%d%d%d%d", &num1, &num2, &num3, &num4);
-
-printf("Maximum is %d\n", MaxNumber(num1, num2, num3, num4));
-printf("Minimum is %d\n", MinNumber(num1, num2, num3, num4));
-
-
-   return 0;
+	return 0;
 }
