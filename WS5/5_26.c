b071bc73e1fcc02e5b2a9913267384ea46fdcf18
digi-dan <danny.garcia@sjsu.edu>
2023-03-05 22:14:29 -0800

    Documentation changes

--- WS5/5_26.c
+++ WS5/5_26.c
@@ -1,19 +1,26 @@
+/**
+*	Description: Counts the input length of a string
+*	excluding spaces, periods, exclamation marks, and commas
+*	Input String shall not exceed 50 characters
+*	Creation Date: 03/05/2023
+*	Last Modified Date: 03/05/2023
+**/
 #include <stdio.h>
 #include <string.h>
 
 int main(void) {
    
-   /* Type your code here. */
-char input[51];
-int count = 0;
-int i;
+	/* Type your code here. */
+	char input[51];
+	int count = 0;
+	int i;
 
-fgets(input, 51, stdin);
-
-for(i = 0; input[i] != '\0' && i < 50; i++){
-if(input[i] != ' ' && input[i] != '.' && input[i] != '!' &&input[i] != ',')
-count++;
-}
-printf("%d\n",count);
-   return 0;
+	fgets(input, 51, stdin);
+	/* Counts the number of characters excluding spaces, periods, exclamation marks, and 	commas*/
+	for(i = 0; input[i] != '\0' && i < 50; i++){
+		if(input[i] != ' ' && input[i] != '.' && input[i] != '!' &&input[i] != ',')
+			count++;
+	}
+	printf("%d\n",count);
+	return 0;
 }
--- WS5/6_28.c
+++ WS5/6_28.c
@@ -1,28 +1,40 @@
+/**
+*	Description: Given a string of up to 50 characters, this program will
+*	print out the given string after removing all non-alphabetic characters
+*	Creation Date: 03/05/2023
+*	Last Modified Date: 03/05/2023
+*	Email: danny.garcia@sjsu.edu
+**/
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
 
 /* Define your function here */
+// Function will move all alphabetic characters from userString to userStringAlphaOnly
 void RemoveNonAlpha(char userString[], char userStringAlphaOnly[]){
-int i, j;
-for(i = 0, j = 0; userString[i] != '\0' && i < 50; i++){
-if(isalpha(userString[i])){
-userStringAlphaOnly[j] = userString[i];
-j++;
+	int i, j;
+	// Iterate through userString array
+	for(i = 0, j = 0; userString[i] != '\0' && i < 50; i++){
+		// If character is alphabetic, add to userStringAlphaOnly
+		if(isalpha(userString[i])){
+			userStringAlphaOnly[j] = userString[i];
+			j++;
+		}
+	}
+	// Append String with null character
+	userStringAlphaOnly[j] = '\0';
 }
-}
-userStringAlphaOnly[j] = '\0';
-}
+
 int main(void) {
 
-   /* Type your code here. */
-char userString[51];
-char userStringAlphaOnly[51];
+	/* Type your code here. */
+	char userString[51];
+	char userStringAlphaOnly[51];
 
-fgets(userString, 51, stdin);
+	fgets(userString, 51, stdin);
 
-RemoveNonAlpha(userString, userStringAlphaOnly);
-printf("%s\n", userStringAlphaOnly);
+	RemoveNonAlpha(userString, userStringAlphaOnly);
+	printf("%s\n", userStringAlphaOnly);
 
-   return 0;
+	return 0;
 }
